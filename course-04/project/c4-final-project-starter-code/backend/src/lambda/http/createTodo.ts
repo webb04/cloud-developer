a18c6203c2f0223d0bef3a4da620aa997e405766
webb04 <jamie.webb@warwick.ac.uk>
2021-03-05 21:10:56 +0000

    cors support

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -37,6 +37,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+    },
     body: JSON.stringify({
       item
     })
